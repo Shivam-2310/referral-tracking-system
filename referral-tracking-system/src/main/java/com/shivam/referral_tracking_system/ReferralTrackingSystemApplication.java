e9040d83d80bad98a91716cdd2ac7363d95d4b98
Shivam-2310 <nshivam2003@gmail.com>
2025-03-29 03:29:34 +0530

    Add OpenAPI/Swagger configuration for API documentation

--- referral-tracking-system/src/main/java/com/shivam/referral_tracking_system/ReferralTrackingSystemApplication.java
+++ referral-tracking-system/src/main/java/com/shivam/referral_tracking_system/ReferralTrackingSystemApplication.java
@@ -1,9 +1,12 @@
 package com.shivam.referral_tracking_system;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Referral Tracking API", version = "1.0", description = "API documentation for the Referral Tracking System"))
 public class ReferralTrackingSystemApplication {
 
 	public static void main(String[] args) {
