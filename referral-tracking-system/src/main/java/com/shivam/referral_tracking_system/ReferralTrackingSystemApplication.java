2cc231dfbd717509a6fad306e115cd25ba0106d3
Shivam-2310 <nshivam2003@gmail.com>
2025-03-28 22:42:10 +0530

    Initial project setup: Create Spring Boot project with base dependencies

---
+++ referral-tracking-system/src/main/java/com/shivam/referral_tracking_system/ReferralTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.shivam.referral_tracking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReferralTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReferralTrackingSystemApplication.class, args);
+	}
+
+}
---
+++ referral-tracking-system/src/test/java/com/shivam/referral_tracking_system/ReferralTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivam.referral_tracking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReferralTrackingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
